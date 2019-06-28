87caeb6ac908ea2bd75661f2fb10f0932622a91d
kennethn <ken@kennethnorton.com>
2019-06-28 15:53:17 -0700

    Final cleanup

--- p2_image_classifier/predict.py
+++ p2_image_classifier/predict.py
@@ -1,6 +1,4 @@
-'''
-Author: Ken Norton
-
+""" predict.py
 Predict flower name from an image with predict.py along with the probability of
 that name. That is, you'll pass in a single image /path/to/image and return the
 flower name and class probability.
@@ -27,136 +25,138 @@ Examples:
 
     Use GPU for inference:
         python predict.py input checkpoint --gpu
-'''
+"""
+__author__ = "Ken Norton <ken@kennethnorton.com>"
 
 import json
 import argparse
-from collections import OrderedDict
-
 import numpy as np
-from PIL import Image
-
 import torch
-from torch import nn
-from torch import optim
 import torch.nn.functional as F
-import torchvision
-from torchvision import datasets, transforms, models
 
-parser = argparse.ArgumentParser(
-    description='Predict flower name from an image with along with the \
-    probability of that name. ')
+from PIL import Image
+from torchvision import transforms, models
 
-parser.add_argument('--img',
-                    type=str,
-                    dest='image_path',
-                    default='flowers/train/1/image_06770.jpg',
-                    action='store',
-                    help='File path to an image input')
-parser.add_argument('--checkpoint',
-                    default='model_checkpoint.pth',
-                    type=str,
-                    dest='checkpoint',
-                    action='store',
-                    help='Model checkpoint')
-parser.add_argument('--top_k',
-                    dest='top_k',
-                    type=int,
-                    default=5,
-                    action='store',
-                    help='Number of top classes to return')
-parser.add_argument('--category_names',
-                    type=str,
-                    dest='category_names',
-                    default='cat_to_name.json',
-                    action='store',
-                    help='JSON file containing category-name mapping')
-parser.add_argument('--gpu',
-                    type=bool,
-                    dest='gpu',
-                    default=True,
-                    action='store',
-                    help='Number of epochs')
 
-pa = parser.parse_args()
-path_to_img = pa.image_path
-checkpoint = pa.checkpoint
-top_k = pa.top_k
-category_names = pa.category_names
-gpu = pa.gpu
+def main():
+    parser = argparse.ArgumentParser(
+        description='Predict flower name from an image with along with the \
+        probability of that name. ')
 
-if (torch.cuda.is_available() and gpu):
-    gpu = True
-    device = torch.device('cuda:0')
-else:
-    gpu = False
-    device = torch.device('cpu')
+    parser.add_argument('--img',
+                        type=str,
+                        dest='image_path',
+                        default='flowers/train/1/image_06770.jpg',
+                        action='store',
+                        help='File path to an image input')
+    parser.add_argument('--checkpoint',
+                        default='model_checkpoint.pth',
+                        type=str,
+                        dest='checkpoint',
+                        action='store',
+                        help='Model checkpoint')
+    parser.add_argument('--top_k',
+                        dest='top_k',
+                        type=int,
+                        default=5,
+                        action='store',
+                        help='Number of top classes to return')
+    parser.add_argument('--category_names',
+                        type=str,
+                        dest='category_names',
+                        default='cat_to_name.json',
+                        action='store',
+                        help='JSON file containing category-name mapping')
+    parser.add_argument('--gpu',
+                        type=bool,
+                        dest='gpu',
+                        default=True,
+                        action='store',
+                        help='Number of epochs')
 
-with open(category_names, 'r') as f:
-    cat_to_name = json.load(f)
+    pa = parser.parse_args()
+    path_to_img = pa.image_path
+    checkpoint = pa.checkpoint
+    top_k = pa.top_k
+    category_names = pa.category_names
+    gpu = pa.gpu
 
-def load_checkpoint(filepath):
-    cp = torch.load(filepath)
-
-    if cp['architecture'] == 'vgg16':
-        model = models.vgg16(pretrained=True)
-        model.name = 'vgg16'
-    elif cp['architecture'] == 'alexnet':
-        model = models.alexnet(pretrained = True)
-        model.name = 'alexnet'
-
-    # Freeze parameters so we don't backprop through them
-    for param in model.parameters():
-        param.requires_grad = False
-
-    model.class_to_idx = cp['class_to_idx']
-    model.classifier = cp['classifier']
-    model.load_state_dict(cp['state_dict'])
-
-    return model
-
-def process_image(image):
-    ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
-        returns a NumPy array
-    '''
-    img_handler = Image.open(image)
-    process_img = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                             std=[0.229, 0.224, 0.225])
-    ])
-    return process_img(img_handler)
-
-def predict(image_path, model):
-    model.to(device)
-    img_torch = process_image(image_path)
-    img_torch = img_torch.unsqueeze_(0)
-    img_torch = img_torch.float()
-
-    with torch.no_grad():
-        output = model.forward(img_torch.cuda())
-
-    probability = F.softmax(output.data, dim=1)
-
-    pb, cl = probability.topk(top_k)
-    if gpu:
-        return pb.cpu().numpy(), cl.cpu().numpy()
+    if (torch.cuda.is_available() and gpu):
+        gpu = True
+        device = torch.device('cuda:0')
     else:
-        return pb.numpy(), cl.numpy()
+        gpu = False
+        device = torch.device('cpu')
 
-def print_predict(image_path, model):
-    probs, classes = predict(image_path, model)
-    probabilities = probs[0]
-    class_names = {val: key for key, val in model.class_to_idx.items()}
-    c_names = [cat_to_name[class_names[x]] for x in classes[0]]
-    index = np.arange(len(c_names))
-    print('Predictions for ', image_path, ':')
-    for i in index:
-        prob = "{0:.2f}".format(probabilities[i] * 100)
-        print(prob, '% -- ', c_names[i])
+    with open(category_names, 'r') as f:
+        cat_to_name = json.load(f)
 
-model = load_checkpoint(checkpoint)
+    def load_checkpoint(filepath):
+        cp = torch.load(filepath)
 
-print_predict(path_to_img, model)
+        if cp['architecture'] == 'vgg16':
+            model = models.vgg16(pretrained=True)
+            model.name = 'vgg16'
+        elif cp['architecture'] == 'alexnet':
+            model = models.alexnet(pretrained=True)
+            model.name = 'alexnet'
+
+        # Freeze parameters so we don't backprop through them
+        for param in model.parameters():
+            param.requires_grad = False
+
+        model.class_to_idx = cp['class_to_idx']
+        model.classifier = cp['classifier']
+        model.load_state_dict(cp['state_dict'])
+
+        return model
+
+    def process_image(image):
+        ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
+            returns a NumPy array
+        '''
+        img_handler = Image.open(image)
+        process_img = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+        ])
+        return process_img(img_handler)
+
+    def predict(image_path, model):
+        model.to(device)
+        img_torch = process_image(image_path)
+        img_torch = img_torch.unsqueeze_(0)
+        img_torch = img_torch.float()
+
+        with torch.no_grad():
+            output = model.forward(img_torch.cuda())
+
+        probability = F.softmax(output.data, dim=1)
+
+        pb, cl = probability.topk(top_k)
+        if gpu:
+            return pb.cpu().numpy(), cl.cpu().numpy()
+        else:
+            return pb.numpy(), cl.numpy()
+
+    def print_predict(image_path, model):
+        probs, classes = predict(image_path, model)
+        probabilities = probs[0]
+        class_names = {val: key for key, val in model.class_to_idx.items()}
+        c_names = [cat_to_name[class_names[x]] for x in classes[0]]
+        index = np.arange(len(c_names))
+        print('Predictions for ', image_path, ':')
+        for i in index:
+            prob = "{0:.2f}".format(probabilities[i] * 100)
+            print(prob, '% -- ', c_names[i])
+
+    model = load_checkpoint(checkpoint)
+
+    print_predict(path_to_img, model)
+
+
+if __name__ == "__main__":
+    main()
--- p2_image_classifier/train.py
+++ p2_image_classifier/train.py
@@ -1,12 +1,11 @@
-'''
-@Author: Ken Norton
-
+""" train.py
 Train a new network on a data set with train.py
 
 Basic usage:
     python train.py data_directory
 
-Prints out training loss, validation loss, and validation accuracy as the network trains
+Prints out training loss, validation loss, and validation accuracy as the
+network trains
 
 Options:
 
@@ -17,245 +16,251 @@ Choose architecture:
     python train.py data_dir --arch "vgg13"
 
 Set hyperparameters:
-    python train.py data_dir --learning_rate 0.001 --hidden_units 512 --epochs 20
+    python train.py data_dir --learning_rate 0.001 --hidden_units 512 \
+        --epochs 20
 
 Use GPU for training:
     python train.py data_dir --gpu
-'''
+"""
+__author__ = "Ken Norton <ken@kennethnorton.com>"
 
 import os
 import json
 import argparse
-from collections import OrderedDict
-
-import numpy as np
-from PIL import Image
-
 import torch
+
+from collections import OrderedDict
 from torch import nn
 from torch import optim
-import torch.nn.functional as F
-import torchvision
 from torchvision import datasets, transforms, models
 
-parser = argparse.ArgumentParser(
-    description='Train a new network on a data set')
 
-parser.add_argument('--data_dir',
-                    type=str,
-                    dest='data_dir',
-                    default='flowers',
-                    action='store',
-                    help='File path to data set')
-parser.add_argument('--save_dir',
-                    default='',
-                    type=str,
-                    dest='save_dir',
-                    action='store',
-                    help='Destination of saved checkpoint')
-parser.add_argument('--arch',
-                    dest='arch',
-                    type=str,
-                    default='vgg16',
-                    action='store',
-                    help='Pretrained model architecture to use')
-parser.add_argument('--learning_rate',
-                    type=float,
-                    dest='learning_rate',
-                    default=0.001,
-                    action='store',
-                    help='Learning rate')
-parser.add_argument('--epochs',
-                    type=int,
-                    dest='epochs',
-                    default=5,
-                    action='store',
-                    help='Number of epochs')
-parser.add_argument('--hidden_units',
-                    type=int,
-                    dest='hidden_units',
-                    default=120,
-                    action='store',
-                    help='Number of hidden units')
-parser.add_argument('--gpu',
-                    type=bool,
-                    dest='gpu',
-                    default=True,
-                    action='store',
-                    help='Number of epochs')
+def main():
+    parser = argparse.ArgumentParser(
+        description='Train a new network on a data set')
 
-pa = parser.parse_args()
-data_dir = pa.data_dir
-save_dir = pa.save_dir
-arch = pa.arch
-learning_rate = pa.learning_rate
-epochs = pa.epochs
-hidden_units = pa.hidden_units
-gpu = pa.gpu
+    parser.add_argument('--data_dir',
+                        type=str,
+                        dest='data_dir',
+                        default='flowers',
+                        action='store',
+                        help='File path to data set')
+    parser.add_argument('--save_dir',
+                        default='',
+                        type=str,
+                        dest='save_dir',
+                        action='store',
+                        help='Destination of saved checkpoint')
+    parser.add_argument('--arch',
+                        dest='arch',
+                        type=str,
+                        default='vgg16',
+                        action='store',
+                        help='Pretrained model architecture to use')
+    parser.add_argument('--learning_rate',
+                        type=float,
+                        dest='learning_rate',
+                        default=0.001,
+                        action='store',
+                        help='Learning rate')
+    parser.add_argument('--epochs',
+                        type=int,
+                        dest='epochs',
+                        default=5,
+                        action='store',
+                        help='Number of epochs')
+    parser.add_argument('--hidden_units',
+                        type=int,
+                        dest='hidden_units',
+                        default=120,
+                        action='store',
+                        help='Number of hidden units')
+    parser.add_argument('--gpu',
+                        type=bool,
+                        dest='gpu',
+                        default=True,
+                        action='store',
+                        help='Number of epochs')
 
-train_dir = data_dir + '/train'
-valid_dir = data_dir + '/valid'
-test_dir = data_dir + '/test'
+    pa = parser.parse_args()
+    data_dir = pa.data_dir
+    save_dir = pa.save_dir
+    arch = pa.arch
+    learning_rate = pa.learning_rate
+    epochs = pa.epochs
+    hidden_units = pa.hidden_units
+    gpu = pa.gpu
 
-# Define transforms for the training data and testing data
-train_transforms = transforms.Compose([transforms.RandomRotation(30),
-                                       transforms.RandomResizedCrop(224),
-                                       transforms.RandomHorizontalFlip(),
-                                       transforms.ToTensor(),
-                                       transforms.Normalize([0.485, 0.456, 0.406],
-                                                            [0.229, 0.224, 0.225])])
+    train_dir = data_dir + '/train'
+    valid_dir = data_dir + '/valid'
+    test_dir = data_dir + '/test'
 
-test_transforms = transforms.Compose([transforms.Resize(256),
-                                      transforms.CenterCrop(224),
-                                      transforms.ToTensor(),
-                                      transforms.Normalize([0.485, 0.456, 0.406],
-                                                           [0.229, 0.224, 0.225])])
+    # Define transforms for the training data and testing data
+    train_transforms = transforms.Compose([
+        transforms.RandomRotation(30),
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
 
-# Pass transforms in here, then run the next cell to see how the transforms look
-train_data = datasets.ImageFolder(train_dir, transform=train_transforms)
-valid_data = datasets.ImageFolder(valid_dir, transform=test_transforms)
-test_data = datasets.ImageFolder(test_dir, transform=test_transforms)
+    test_transforms = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
 
-trainloader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
-validloader = torch.utils.data.DataLoader(valid_data, batch_size=32)
-testloader = torch.utils.data.DataLoader(test_data, batch_size=32)
+    # Pass transforms in here, then run the next cell to see how the
+    # transforms look
+    train_data = datasets.ImageFolder(train_dir, transform=train_transforms)
+    valid_data = datasets.ImageFolder(valid_dir, transform=test_transforms)
+    test_data = datasets.ImageFolder(test_dir, transform=test_transforms)
 
-class_names = train_data.classes
-output_size = len(class_names)
+    trainloader = torch.utils.data.DataLoader(
+        train_data, batch_size=64, shuffle=True)
+    validloader = torch.utils.data.DataLoader(valid_data, batch_size=32)
+    testloader = torch.utils.data.DataLoader(test_data, batch_size=32)
 
-with open('cat_to_name.json', 'r') as f:
-    cat_to_name = json.load(f)
+    class_names = train_data.classes
+    output_size = len(class_names)
 
-if arch == 'vgg16':
-    model = models.vgg16(pretrained=True)
-    model.name = 'vgg16'
-    input_size = 25088
-elif arch == 'alexnet':
-    model = models.alexnet(pretrained = True)
-    model.name = 'alexnet'
-    input_size = 9216
-else:
-    print('ERROR:', arch, 'is not a valid model architecture')
-    exit()
+    with open('cat_to_name.json', 'r') as f:
+        cat_to_name = json.load(f)
 
-print('Loading', arch, 'pretrained model...........................')
+    if arch == 'vgg16':
+        model = models.vgg16(pretrained=True)
+        model.name = 'vgg16'
+        input_size = 25088
+    elif arch == 'alexnet':
+        model = models.alexnet(pretrained=True)
+        model.name = 'alexnet'
+        input_size = 9216
+    else:
+        print('ERROR:', arch, 'is not a valid model architecture')
+        exit()
 
-# Freeze parameters so we don't backprop through them
-for param in model.parameters():
-    param.requires_grad = False
+    print('Loading', arch, 'pretrained model...........................')
 
-classifier = nn.Sequential(OrderedDict([
-            ('dropout',nn.Dropout(0.5)),
-            ('inputs', nn.Linear(input_size, hidden_units)),
-            ('relu1', nn.ReLU()),
-            ('hidden_layer1', nn.Linear(hidden_units, 90)),
-            ('relu2',nn.ReLU()),
-            ('hidden_layer2',nn.Linear(90, 80)),
-            ('relu3',nn.ReLU()),
-            ('hidden_layer3',nn.Linear(80, output_size)),
-            ('output', nn.LogSoftmax(dim=1))
-            ]))
+    # Freeze parameters so we don't backprop through them
+    for param in model.parameters():
+        param.requires_grad = False
 
-model.classifier = classifier
+    classifier = nn.Sequential(OrderedDict([
+        ('dropout', nn.Dropout(0.5)),
+        ('inputs', nn.Linear(input_size, hidden_units)),
+        ('relu1', nn.ReLU()),
+        ('hidden_layer1', nn.Linear(hidden_units, 90)),
+        ('relu2', nn.ReLU()),
+        ('hidden_layer2', nn.Linear(90, 80)),
+        ('relu3', nn.ReLU()),
+        ('hidden_layer3', nn.Linear(80, output_size)),
+        ('output', nn.LogSoftmax(dim=1))
+    ]))
 
-if (torch.cuda.is_available() and gpu):
-    device = torch.device('cuda:0')
-else:
-    device = torch.device('cpu')
+    model.classifier = classifier
 
-print('Using', device, '...........................................')
+    if (torch.cuda.is_available() and gpu):
+        device = torch.device('cuda:0')
+    else:
+        device = torch.device('cpu')
 
-model.to(device)
+    print('Using', device, '...........................................')
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.classifier.parameters(), lr=learning_rate)
-
-print_every = 5
-steps = 0
-running_loss = 0
-
-print('Training model..............................................')
-
-for e in range(epochs):
-    for ii, (inputs, labels) in enumerate(trainloader):
-        steps += 1
-
-        # Move input and label tensors to the default device
-        inputs, labels = inputs.to(device), labels.to(device)
-
-        optimizer.zero_grad()
-
-        outputs = model.forward(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        running_loss += loss.item()
-
-        if steps % print_every == 0:
-            model.eval()
-            loss_v = 0
-            accuracy = 0
-
-            for ii, (inputs_v, labels_v) in enumerate(validloader):
-                optimizer.zero_grad()
-
-                inputs_v, labels_v = inputs_v.to(device), labels_v.to(device)
-                model.to(device)
-                with torch.no_grad():
-                    outputs = model.forward(inputs_v)
-                    loss_v = criterion(outputs, labels_v)
-                    ps = torch.exp(outputs).data
-                    equals = (labels_v.data == ps.max(1)[1])
-                    accuracy += equals.type_as(torch.FloatTensor()).mean()
-
-            loss_v = loss_v / len(validloader)
-            accuracy = accuracy / len(validloader)
-
-            print(f"Epoch {e+1}/{epochs}.. "
-                  f"Train loss: {running_loss/print_every:.3f}.. "
-                  f"Validation loss: {loss_v:.3f}.. "
-                  f"Accuracy: {accuracy:.3f}")
-
-            running_loss = 0
-
-
-# Do validation on the test set
-def test_network(testloader):
-    correct = 0
-    total = 0
     model.to(device)
-    with torch.no_grad():
-        for data in testloader:
-            images, labels = data
-            images, labels = images.to(device), labels.to(device)
-            outputs = model(images)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
 
-    print('Test accuracy: %d%%' % (100 * correct / total))
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.classifier.parameters(), lr=learning_rate)
+
+    print_every = 5
+    steps = 0
+    running_loss = 0
+
+    print('Training model..............................................')
+
+    for e in range(epochs):
+        for ii, (inputs, labels) in enumerate(trainloader):
+            steps += 1
+
+            # Move input and label tensors to the default device
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            optimizer.zero_grad()
+
+            outputs = model.forward(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+            if steps % print_every == 0:
+                model.eval()
+                loss_v = 0
+                accuracy = 0
+
+                for ii, (inputs_v, labels_v) in enumerate(validloader):
+                    optimizer.zero_grad()
+
+                    inputs_v, labels_v = inputs_v.to(
+                        device), labels_v.to(device)
+                    model.to(device)
+                    with torch.no_grad():
+                        outputs = model.forward(inputs_v)
+                        loss_v = criterion(outputs, labels_v)
+                        ps = torch.exp(outputs).data
+                        equals = (labels_v.data == ps.max(1)[1])
+                        accuracy += equals.type_as(torch.FloatTensor()).mean()
+
+                loss_v = loss_v / len(validloader)
+                accuracy = accuracy / len(validloader)
+
+                print(f"Epoch {e+1}/{epochs}.. "
+                      f"Train loss: {running_loss/print_every:.3f}.. "
+                      f"Validation loss: {loss_v:.3f}.. "
+                      f"Accuracy: {accuracy:.3f}")
+
+                running_loss = 0
+
+    print('Validating on test set..........................................')
+
+    # Do validation on the test set
+    def test_network(testloader):
+        correct = 0
+        total = 0
+        model.to(device)
+        with torch.no_grad():
+            for data in testloader:
+                images, labels = data
+                images, labels = images.to(device), labels.to(device)
+                outputs = model(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+        print('Test accuracy: %d%%' % (100 * correct / total))
+
+    test_network(testloader)
+
+    model_f = os.path.join(save_dir, 'model_checkpoint.pth')
+
+    model.class_to_idx = trainloader.dataset.class_to_idx
+    model.epochs = epochs
+    checkpoint = {'architecture': arch,
+                  'batch_size': trainloader.batch_size,
+                  'input_size': input_size,
+                  'output_size': output_size,
+                  'hidden_units': hidden_units,
+                  'learning_rate': learning_rate,
+                  'state_dict': model.state_dict(),
+                  'optimizer_dict': optimizer.state_dict(),
+                  'class_to_idx': model.class_to_idx,
+                  'epoch': model.epochs,
+                  'classifier': model.classifier}
+
+    print('Saving model checkpoint.........................................')
+    torch.save(checkpoint, model_f)
 
 
-print('Validating on test set..........................................')
-
-test_network(testloader)
-
-model_f = os.path.join(save_dir, 'model_checkpoint.pth')
-
-model.class_to_idx = trainloader.dataset.class_to_idx
-model.epochs = epochs
-checkpoint = {'architecture': arch,
-              'batch_size': trainloader.batch_size,
-              'input_size': input_size,
-              'output_size': output_size,
-              'hidden_units': hidden_units,
-              'learning_rate': learning_rate,
-              'state_dict': model.state_dict(),
-              'optimizer_dict': optimizer.state_dict(),
-              'class_to_idx': model.class_to_idx,
-              'epoch': model.epochs,
-              'classifier': model.classifier}
-
-print('Saving model checkpoint.........................................')
-torch.save(checkpoint, model_f)
+if __name__ == "__main__":
+    main()
